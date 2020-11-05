bf2f755d9b43f9893648ed53e19825649caaf26a
Blake Vente <venteralph@gmail.com>
2020-11-05 14:28:32 -0500

    remove needless copy to vector

--- grad/reg_driver.cpp
+++ grad/reg_driver.cpp
@@ -1,3 +1,4 @@
+/* */
 #include "logistic_regression.hpp"
 #include "xtensor/xadapt.hpp"
 
@@ -11,11 +12,12 @@ int main(const int argc, const char* argv[])
     train_fname, test_fname, N_ITER, LEARNING_RATE
     ] = std::make_tuple(argv[1], argv[2], atoi(argv[3]), std::stod(argv[4]));
 
-  std::ifstream train(train_fname);
+  // load csv from files
+  std::ifstream train(train_fname); // training set
   auto training_set_raw = xt::load_csv<double>(train);
   train.close();
 
-  std::ifstream test(test_fname);
+  std::ifstream test(test_fname); // test set
   auto test_set_raw = xt::load_csv<double>(test);
   test.close();
 
@@ -28,17 +30,16 @@ int main(const int argc, const char* argv[])
   IC(train_labels, training_set);
   auto weights  = logistic_regression(training_set, train_labels, N_ITER, LEARNING_RATE);
   IC(weights);
+  IC(weights.shape());
   auto scores = xt::linalg::dot(xt::eval(test_set), weights);
 
   auto predictions = 1.0/(1.0 + xt::exp(-1 * scores));
   auto a  = xt::eval(test_labels);
   auto p =  xt::eval(predictions > .5);
 
-  std::vector<double> actual(a.begin(), a.end());
-  std::vector<bool> bool_predictions_vec(p.begin(), p.end());
   double sum = 0;
   for (int i = 0; i < p.size(); ++i) {
-    sum += (actual[i] == p[i]);
+    sum += (a[i] == p[i]);
   }
   auto accuracy = sum/p.size();
   IC(accuracy);
