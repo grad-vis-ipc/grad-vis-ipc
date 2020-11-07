65747ac0fa98bb8c8718bcaaf4b84ce7337c078f
liulanz <zhengliulan@gmail.com>
2020-11-06 23:10:40 -0500

    add Makefile, minor change in ipc

--- ipc/rscode.rs
+++ ipc/rscode.rs
@@ -3,7 +3,8 @@ use std::io::prelude::*;
 
 fn main() {
     let stdin = io::stdin();
+    println!("This is rust code. I received: ");
     for line in stdin.lock().lines() {
-        println!("{}", line.unwrap());
+        println!("{}", line.unwrap()+"<<");
     }
 }
