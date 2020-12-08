6c827b5cdd735899ece75dc9be567f417917f7e1
liulanz <zhengliulan@gmail.com>
2020-12-08 02:53:41 -0500

    combine ipc and logistic regression

--- ipc/LogReg.cpp
+++ ipc/LogReg.cpp
@@ -16,18 +16,19 @@
 
 #include <unistd.h>
 #include <sys/shm.h> 
+#include <sys/wait.h> 
 #include <fcntl.h>
 #include <string>
 #include <vector>
 
+using namespace std;
+#define GRAD_READ pipeML3D[0]
+#define GRAD_WRITE pipeML3D[1]
 
-// #define GRAD_READ pipeML3D[0]
-// #define GRAD_WRITE pipeML3D[1]
-
-// struct pcb{
-//     pid_t pid;
-//     pcb* next;
-// };
+struct pcb{
+    pid_t pid;
+    pcb* next;
+};
 
 
 using d_vec = xt::xarray<double>;
@@ -87,20 +88,48 @@ struct LogReg {
 };
 
 int main(const int argc, const char* argv[]) {
+   if(argc != 2){
+        cout << "Usage: " << argv[0] << " <readfile>" << endl;
+        return EXIT_FAILURE;       
+    }
+    int* pids;
+    int pipeML3D[2];
+    int *ready;
+    int shmid1, shmid2;  
+    int i;
+    pid_t receive_pid;
+    int status;  
+    pcb * head;   
+    pid_t pid;
+    key_t key = ftok("shmfile",65); 
+    shmid1 = shmget (key, sizeof (int), IPC_CREAT | 0666);
+    if (shmid1 < 0) {                           /* shmget error check */
+        cout << "shmget error" << endl;
+        exit (1);
+    } 
+    /* Attach shared variable to shared memory using shmat  */
+    ready = (int *) shmat (shmid1, NULL, 0);   
+    if(*ready == -1){
+        cout << "Attachment errr" << endl;
+        exit(1);
+    }
+    *ready = 0;
+    if(pipe(pipeML3D) == -1 ){  
+        perror("Pipe creation failure.");
+        exit(EXIT_FAILURE);
+    }
+    
+   
 
-
-   key_t key = ftok("shmfile",65); 
-   int shmid = shmget(key, sizeof (int),0666|IPC_CREAT); // shmget returns an ide in shmid
-   int *ready = (int*) shmat(shmid,(void*)0,0); // shmat to join to shared memory
-
+ 
   // icecream::ic.prefix("DEBUG| ").line_wrap_width(80);
   // if (argc < 5) {
   //   std::cout << "Usage: " << argv[0] << " <train> <test> <iterations> <step>"
   //             << std::endl;
   //   return EXIT_FAILURE;
   // }
-  const auto [train_fname, test_fname, N_ITER, LEARNING_RATE] =
-      std::make_tuple(argv[1], argv[2], atoi(argv[3]), std::stod(argv[4]));
+  const auto [ N_ITER, LEARNING_RATE] =
+      std::make_tuple(1000, 1e-19);
 
   // create two classes A and B which are normally distributed
   auto A = xt::random::randn<double>({100, 3}, 1, .3);
@@ -127,23 +156,80 @@ int main(const int argc, const char* argv[]) {
   auto test_set = xt::vstack(xt::xtuple(A_test, B_test));
 
   auto clf = LogReg(e_features, e_labels, N_ITER, LEARNING_RATE);
-  for (int i = 0; i < 10; ++i) {
 
-    auto predictions = clf.predict(test_set);
-    // IC(predictions);
 
-    // take a single step in the classifier
-    clf.train_step();
-
-    // print out all new predictions in form x y z LABEL
-    for (int j = 0; j < 40; ++j) {
-      auto row = xt::view(test_set, j, xt::all());
-      auto pred = xt::view(predictions, xt::all(), j);
-      auto t_vec = std::vector<double>(row.begin(), row.end());
-      auto p_vec = std::vector<double>(pred.begin(), pred.end());
-      std::cout << t_vec[0] << " " << t_vec[1] << " " << t_vec[2] << " " << p_vec[0] << std::endl;
+  pid = fork();
+    if (pid < 0) {       /* Error check */    
+        cout << "fork error" << endl;
+        exit(1);
     }
-  }
+    else{
+       receive_pid = pid;
+    }
+    
+    if (pid > 0){ /* Parent Process */ 
+  
+       dup2(GRAD_WRITE , STDOUT_FILENO); /* make output go to pipe */
+      close(GRAD_READ);
+      //  string data;
+      
+        while(true){
+       
+
+            auto predictions = clf.predict(test_set);
+            // IC(predictions);
+
+            // take a single step in the classifier
+            clf.train_step();
+
+            // print out all new predictions in form x y z LABEL
+            for (int j = 0; j < 40; ++j) {
+              while(*ready!=1){}
+              auto row = xt::view(test_set, j, xt::all());
+              auto pred = xt::view(predictions, xt::all(), j);
+              auto t_vec = std::vector<double>(row.begin(), row.end());
+              auto p_vec = std::vector<double>(pred.begin(), pred.end());
+              std::cout << t_vec[0] << " " << t_vec[1] << " " << t_vec[2] << " " << p_vec[0] << std::endl;
+              *ready = 0;
+            }
+        }
+        
+
+        close(GRAD_WRITE);
+        close(GRAD_READ);
+ 
+        /* wait until read finishes reading the rest of the data */
+        while((status = waitpid(receive_pid, &status, WNOHANG|WUNTRACED))) {
+          if(status == receive_pid )
+            break;
+            else if(status==-1)
+                return -1;
+            else if(status == 0){
+              sleep(1);
+            }
+        }
+        
+        /* deatach shared memory */
+        status = shmdt (ready);
+        if(status == -1){
+            cout << "Error: Remove shared memory segment  " << endl;
+            exit(1);
+        }   
+        status = shmctl (shmid1, IPC_RMID, 0); 
+        if(status == -1){
+            cout << "Error: Remove shared memory segment  " << endl;
+            exit(1);
+        }   
+        exit(0);
+    }
+    else{   /* Child process */  
+      close(GRAD_WRITE);
+        dup2(GRAD_READ,STDIN_FILENO);  /* get input from pipe */
+        execlp(argv[1],argv[1], NULL);   
+   }
+        
+
+  
 
   return EXIT_SUCCESS;
 }
